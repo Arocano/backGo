88e6622efaeface4ab31d174d8c0096662914c0e
Alejo <arocano6813@uta.edu.ec>
2023-02-06 21:11:58 -0500

    ss

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ config()
 export const PORT=process.env.PORT||3000
 export const DB_USER=process.env.DB_USER|| 'root'
 export const DB_PASSWORD=process.env.DB_PASSWORD|| 'Ad.12345'
-export const DB_HOST=process.env.DB_HOST|| '34.132.217.122'
+export const DB_HOST=process.env.DB_HOST|| '35.188.177.192'
 export const DB_DATABASE=process.env.DB_DATABASE|| 'TaxiGo'
 export const DB_PORT=process.env.DB_PORT|| 3306
